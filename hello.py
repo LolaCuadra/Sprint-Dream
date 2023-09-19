2175cadf27aa501dd4b5e5a8734a9000d1edfcd3
Alex Giraldo <gir15002@byui.edu>
2023-09-19 09:06:46 -0600

    .

--- hello.py
+++ hello.py
@@ -1,5 +1,5 @@
 print("hello this is lola")
-print("Hello this is Alex")
+print("Hello, this is Alex")
 print("Hello this is Joseph")
 print("Hello this is Christian")
 print("Hello. This is Shaun.")
