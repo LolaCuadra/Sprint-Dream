dd2a772d3e5fae099905a31273829b0a61160dcb
LolaCuadra <lola_cuadra1940@byui.edu>
2023-12-09 14:11:12 -0700

    push

--- hello.py
+++
@@ -1,6 +0,0 @@
-print("hello this is lola")
-print("Hello, this is Alex")
-print("Hello this is Joseph")
-print("Hello this is Christian")
-print("Hello. This is Shaun.")
-print("hello, this is Josue")
